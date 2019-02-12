732d09cdcbfc766c9176d49fc4638e0766f215f8
spookycz <42183766+spookycz@users.noreply.github.com>
2019-02-12 23:21:22 +0100

    Init
    Init

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var alzaKPIB4B1B6D1E62543DCB1B68B5EC8979367_DEBUG = {
+        name: 'alzaKPIB4B1B6D1E62543DCB1B68B5EC8979367_DEBUG',
+        displayName: 'AlzaKPI',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.alzaKPIB4B1B6D1E62543DCB1B68B5EC8979367.Visual(options),
+        custom: true
+    };
+}
